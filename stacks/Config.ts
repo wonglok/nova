a7d23a2d8ce5d08c5da47f216d0fd067558e2ae5
Wong Lok <wonglok@users.noreply.github.com>
2023-01-04 12:31:13 +0800

    deploy

--- stacks/Config.ts
+++ stacks/Config.ts
@@ -1,3 +1,3 @@
 export const GOOGLE_CLIENT_ID = `731023934508-nou9ruf23nu9h85s59gu7evr7qq7pkh9.apps.googleusercontent.com`;
-export const PRODUCTION_SITE_URL = `https://admin.agape.town`;
+export const PRODUCTION_SITE_URL = `https://portal.agape.town`;
 export const LOCAL_SITE_URL = `https://wonglok.ap.ngrok.io/`;
